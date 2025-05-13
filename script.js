8f47e4fec31f8f996d0e14e0ebd29f19e83345c2
cappuu02 <lucacapuccini13@gmail.com>
2025-05-13 11:42:14 +0200

    1 push

---
+++ script.js
@@ -0,0 +1,19 @@
+let c = 45;
+
+function draw(){
+  document.documentElement.style.setProperty('--direction', c++ + 'deg');
+  requestAnimationFrame(draw);
+}
+
+requestAnimationFrame(draw);
+
+document.addEventListener('DOMContentLoaded', () => {
+  const background = document.querySelector('.background');
+  const foreground = document.querySelector('.foreground');
+
+  // Cambia l'immagine di sfondo dopo 5 secondi
+  setTimeout(() => {
+    background.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?city')";
+    foreground.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?forest')";
+  }, 5000);
+});
